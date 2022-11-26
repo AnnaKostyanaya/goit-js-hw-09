820c683b7814b91250bb079f332e89355aa81ba5
Hanna Kostiana <anna.kostianaia@gmail.com>
2022-11-26 22:28:28 +0200

    add const to refs

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -27,7 +27,7 @@ const options = {
     },
 };
 
-refs = {
+const refs = {
     startBtn: document.querySelector(`button[data-start]`),
     days:document.querySelector(`span[data-days]`),
     hours:document.querySelector(`span[data-hours]`),
